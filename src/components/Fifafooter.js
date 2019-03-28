39c544c0324528f197690104f125881c91fcc590
huangdonghang <498550830@qq.com>
2019-03-28 10:35:29 +0800

    footer基本完成，common.css增加通用样式

--- src/components/Fifafooter.js
+++ src/components/Fifafooter.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import logo from '~/assets/logo.png';
+import logo from '~/assets/img/logo.svg';
 import * as homeActions from '../redux/reduces/home';
 
 @connect(
@@ -11,28 +11,36 @@ import * as homeActions from '../redux/reduces/home';
 
 class Fifafooter extends Component {
   render() {
+    const grayColor = {
+      color: '#9A9A9A'
+    };
     return (
       <div className="footer">
-        <div>
-          <div>
-            <div>关于我们</div>
-            <div>版权信息</div>
-            <div>新手入门</div>
+        <div className="footer_top flex">
+          <div className="flex_1">
+            <p>关于我们</p>
+            <p>版权信息</p>
+            <p>新手入门</p>
           </div>
-          <div>
-            <div>FIFA资讯</div>
-            <div>FIFA</div>
-            <div>FIFA Online</div>
-            <div>实况足球</div>
-            <div>Football Manager</div>
+          <div className="flex_1">
+            <p>FIFA资讯</p>
+            <p style={grayColor}>FIFA</p>
+            <p style={grayColor}>FIFA Online</p>
+            <p style={grayColor}>实况足球</p>
+            <p style={grayColor}>Football Manager</p>
           </div>
-          <div>
-            <div>电竞赛事</div>
-            <div>FUT Champions</div>
-            <div>FIWC 电子足球世界杯</div>
-            <div>ESWC</div>
-            <div>非凡赛事</div>
+          <div className="flex_1">
+            <p>电竞赛事</p>
+            <p style={grayColor}>FUT Champions</p>
+            <p style={grayColor}>FIWC 电子足球世界杯</p>
+            <p style={grayColor}>ESWC</p>
+            <p style={grayColor}>非凡赛事</p>
           </div>
+        </div>
+        <div className="footer-bottom flex">
+          <div className="flex_1">© 2019 all rights reserved</div>
+          <img src={logo} width={45} height={35} className="flex_1" style={{'margin-top': '-5px'}} />
+          <div>沪ICP备14031707 | 广播电视节目制作经营许可证（泸）字第2467号</div>
         </div>
       </div>
     );
